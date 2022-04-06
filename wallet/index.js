d38d8c722b65419c44a82a1221c4031abc3fd0ca
johnnylarner <james.richardson.2556@gmail,com>
2022-04-06 14:35:19 +0200

    verification and signing

--- wallet/index.js
+++ wallet/index.js
@@ -1,14 +1,21 @@
 const config = require('config');
 const { ec } = require('../util');
+const cryptoHash = require('../util/crypto-hash');
+const hexToBinary = require('hex-to-binary');
+const Signature = require('elliptic/lib/elliptic/ec/signature');
 
 class Wallet {
     constructor() {
         this.balance = config.get('starting-balance');
 
-        const keyPair = ec.genKeyPair();
+        this.keyPair = ec.genKeyPair();
         
-        this.publicKey = keyPair.getPublic().encode('hex');
+        this.publicKey = this.keyPair.getPublic().encode('hex');
     }
-};
+
+    sign( data ) {
+      return this.keyPair.sign(cryptoHash(data));
+    };
+  };
 
 module.exports = Wallet;
